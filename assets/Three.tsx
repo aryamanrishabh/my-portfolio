35674dc6aedded50e4ddbc621e6278aee749be0c
Aryaman Rishabh <aryamanrishabh@gmail.com>
2025-03-17 12:32:46 -0400

    add assets

---
+++ assets/Three.tsx
@@ -0,0 +1,76 @@
+import { GRAY } from "@/lib/colors";
+
+export const Three = () => (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 100 100"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="threejs">
+      <g id="threejs_2">
+        <path
+          id="Vector"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M26.7016 102L2 2.00049L101.023 30.5097L26.7016 102Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M51.4929 16.2579L63.8349 66.2728L14.3511 52.0136L51.4929 16.2579Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_3"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M39.2145 58.7971L33.0844 33.9514L57.6688 41.0087L39.2145 58.7971Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_4"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M26.952 9.13611L33.0821 33.9818L8.49768 26.9245L26.952 9.13611Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_5"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M76.1186 23.2992L82.2487 48.1449L57.6643 41.0876L76.1186 23.2992Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_6"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M39.2173 58.8142L45.3474 83.6599L20.7629 76.6026L39.2173 58.8142Z"
+          stroke={GRAY}
+          strokeWidth="2.2865"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+        />
+      </g>
+    </g>
+  </svg>
+);
